b3051570fde95daa64024f38d74d98e940e87c85
Bruno Perez <brunogperez91@gmail.com>
2024-10-30 17:40:24 -0300

    ADD, authUser & AuthGuard

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AuthData } from '../../features/auth/models';
-import { Observable, of, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
 import { User } from '../../features/dashboard/users/models';
 import { generateRandomString } from '../../shared/utils';
+import { Router } from '@angular/router';
 
 const FAKE_USER: User = {
-  email: 'admin@mail.com',
-  firstName: 'admin',
-  lastName: 'admin',
+  email: 'tutor@mail.com',
+  firstName: 'Sofía',
+  lastName: 'Altamirano',
   id: generateRandomString(8),
   createdAt: new Date(),
   birthdate: new Date(),
@@ -16,10 +17,21 @@ const FAKE_USER: User = {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private _authUser$ = new BehaviorSubject<null | User>(null);
+  public authUser$ = this._authUser$.asObservable();
+
+  constructor(private router: Router){}
+
   login(data: AuthData): Observable<User> {
     if (data.email != FAKE_USER.email || data.password != FAKE_USER.password) {
       return throwError(() => new Error('Los datos son inválidos'));
     }
+    this._authUser$.next(FAKE_USER);
     return of(FAKE_USER);
+  }
+
+  logout() {
+    this._authUser$.next(null);
+    this.router.navigate(['auth','login'])
   }
 }
